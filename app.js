d65562a1ce97147a0a3b936894d555d162041273
harrlin flip <kathgolap909@gmail.com>
2024-09-28 20:08:56 +0600

    api set

---
+++ app.js
@@ -0,0 +1,93 @@
+'use strict'
+
+let catchInput = document.getElementById("inpt");
+let catchBtn = document.getElementById("btn");
+let catchAudioParent = document.getElementById("audioParent");
+let catchSuraTafsir = document.getElementById("suraTafsir");
+let catchAyahNo = document.getElementById("ayahNo")
+let catchJuz = document.getElementById("juz")
+let catchMuljil = document.getElementById("munjil");
+let catchRuku = document.getElementById("ruku");
+let catchSajda = document.getElementById("sajda");
+let catchSuraName = document.getElementById("suraName");
+
+
+function inptValidatio() {
+    let getInptValue = catchInput.value; 
+    if (getInptValue === '') {
+        alert("Input box cannot be blank!");
+    }
+    else if (isNaN(getInptValue)) {
+        alert("Please enter a valid number!");
+    } else if (getInptValue <= 114) {
+        resApi(getInptValue);
+        
+        
+    }
+    else {
+        alert('please enter sura name between 1 to 114')
+    }
+}
+function resApi(recgetInptValue) {
+    let getInput = recgetInptValue
+    const api1 = fetch(`http://api.alquran.cloud/v1/surah/${getInput}/ar.alafasy`);
+    const api2 = fetch(`https://api.alquran.cloud/v1/surah/${getInput}/bn.bengali`);
+
+    Promise.all([api1, api2])
+        .then(recponses => Promise.all(recponses.map(rec => rec.json())))
+        .then(data => apiData(data[0], data[1]))
+    
+}
+function apiData(recData1, recData2) {
+    let finalData1 = recData1;
+    let finalData2 = recData2
+    console.log(recData1);
+    console.log(recData2);
+    
+    let curentAudoLength = finalData1.data.ayahs.length;
+    let curentTafsirLength = recData2.data.ayahs.length;
+    
+    
+    
+    
+    let audioCountFlag = 0;
+    function forAdioWork() {
+        
+        if (audioCountFlag < curentAudoLength && audioCountFlag < curentTafsirLength) {
+            //sura audio play section
+            catchAudioParent.innerHTML = `<audio id="adio" src="${finalData1.data.ayahs[audioCountFlag].audio}" controls autoplay></audio>`
+            //sura name
+            catchSuraName.innerHTML = `<h2>সুরা নাম ${finalData2.data.englishName}</h2>`
+            //sura tafsir print section
+            catchSuraTafsir.innerHTML = `<h2>${finalData2.data.ayahs[audioCountFlag].text}</h2>`
+            //sura info print section
+            // ayah count
+            catchAyahNo.innerHTML = `আয়াত নং ${recData2.data.ayahs[audioCountFlag].numberInSurah}` 
+            //juz
+            catchJuz.innerHTML = `পারা ${recData2.data.ayahs[audioCountFlag].juz}`
+            //manzil
+            catchMuljil.innerHTML = `মঞ্জিল ${recData2.data.ayahs[audioCountFlag].manzil}`
+            //ruku
+            catchRuku.innerHTML = `রুকু ${recData2.data.ayahs[audioCountFlag].ruku}`
+            //sajda
+            catchSajda.innerHTML = `${recData2.data.ayahs[audioCountFlag].sajda ? "কোনো সেজদা আয়াত আছে" : "সেজদা আয়াত নাই"}`
+
+
+
+
+
+            let catchAudio = document.getElementById('adio');
+            catchAudio.onended = () => {
+                audioCountFlag++
+                forAdioWork()
+            }
+        } else {
+            alert("the sura is end would you play it again or another")
+        }
+    }
+    forAdioWork()
+}
+
+catchBtn.addEventListener('click', function () {
+    inptValidatio()
+});
